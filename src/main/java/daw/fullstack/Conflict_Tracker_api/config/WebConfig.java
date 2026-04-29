e2221e096bc2120d2e36b5d09a46cb6d81d8b28e
TadeuArm <tadeuossanbaudelio@elpuig.xeill.net>
2026-04-29 17:23:13 +0200

    cambios para railway(6767667)vercel676767

--- src/main/java/daw/fullstack/Conflict_Tracker_api/config/WebConfig.java
+++ src/main/java/daw/fullstack/Conflict_Tracker_api/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://tu-app-en-vercel.vercel.app", "http://localhost:5173")
+                .allowedOrigins("frontendconflicttracker.vercel.app", "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
